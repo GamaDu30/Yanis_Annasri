e5f2588a770a8b166ee9e59e31bbff67032abb51
Yanis Annasri <yanis.annasri@sfr.fr>
2024-01-31 10:44:00 +0100

    Site Portfolio V2

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ function scrollToPanel(panelId) {
     const panelElement = document.querySelector(`.${panelId}`);
     panelElement.scrollIntoView({ behavior: 'smooth' });
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const animateOnScrollElements = document.querySelectorAll('.slideIn');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            }
+        });
+    }, {
+        rootMargin: '0px',
+        threshold: 0.5
+    });
+
+    animateOnScrollElements.forEach(element => {
+        observer.observe(element);
+    });
+});
